b1b14d48ceb7f87e8edb61be2a922e04f5024862
bibekghimire-ith <bibek.ghimire83@gmail.com>
2024-08-03 17:38:09 +0545

    Encryption of Credentials with CryptoJS

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,5 +1,7 @@
 import { test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
+import { decrypt, encrypt } from "../utils/CryptoJSUtils";
+import { encryptEnvFile, decryptEnvFile } from "../utils/EncryptEnvFiles";
 
 
 // Using existing "page" fixture from playwright
@@ -8,15 +10,17 @@ test("Login test", async ({ page }) => {
 
     await loginPage.navigateToLoginPage();
     // ! is used to make sure not NULL value -> Avoid undefined value / TS Error
-    await loginPage.fillUsername(process.env.username!);
-    await loginPage.fillPassword(process.env.password!);
+    await loginPage.fillUsername(decrypt(process.env.username!));
+    await loginPage.fillPassword(decrypt(process.env.password!));
 
     const homePage = await loginPage.clickLoginButton();
     await homePage.expectHomePageTitleToBeVisible();
 });
 
-// test("ENV test", async ({ page }) => {
+// test.skip("ENV test", async ({ page }) => {
 //     console.log(process.env.NODE_ENV);
 //     console.log(process.env.username);
 //     console.log(process.env.password);
+//     encryptEnvFile()
+
 // })
