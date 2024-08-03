b6ef0e3f615d9c16f9e35f367a651937196d45d8
bibekghimire-ith <bibek.ghimire83@gmail.com>
2024-08-03 14:26:47 +0545

    Configured dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,15 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+if (!process.env.NODE_ENV) {
+  require('dotenv').config({path:`${__dirname}//src//config//.env`});
+} else {
+  // CMD: set NODE_ENV=qa
+  // Terminal: export NODE_ENV="qa" -> printenv NODE_ENV  -> echo $NODE_ENV
+  require('dotenv').config({ path: `${__dirname}//src//config//.env.${process.env.NODE_ENV}` });
+}
+
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -7,9 +7,16 @@ test("Login test", async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.navigateToLoginPage();
-    await loginPage.fillUsername("qa-test@abc.com");
-    await loginPage.fillPassword("Test@12345");
+    // ! is used to make sure not NULL value -> Avoid undefined value / TS Error
+    await loginPage.fillUsername(process.env.username!);
+    await loginPage.fillPassword(process.env.password!);
 
     const homePage = await loginPage.clickLoginButton();
     await homePage.expectHomePageTitleToBeVisible();
 });
+
+// test("ENV test", async ({ page }) => {
+//     console.log(process.env.NODE_ENV);
+//     console.log(process.env.username);
+//     console.log(process.env.password);
+// })
